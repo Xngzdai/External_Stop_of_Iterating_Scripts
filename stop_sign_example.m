285c4aba8d6e8430ffda00c5f761960f4713fe50
Xingze Dai <xngzdai@umich.edu>
2022-02-08 00:44:34 -0500

    Text file example

---
+++ stop_sign_example.m
@@ -0,0 +1,5 @@
+1
+% REWRITE your text file for the stop condition. This is an example of 
+% what your text file (.txt) should be like. Make sure either 1 or 0 is 
+% the first character and no other character is present on the first line.
+% You can change the number from 1 to 0 to stop the iteration.
